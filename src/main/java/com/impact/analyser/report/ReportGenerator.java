91cbac29ad0da8d7b34aa584d07f33080f002d0f
Yuvaraj <yuvaraj.gunisetti@ba.com>
2018-03-11 03:56:37 +0000

    added fix for reporting

--- src/main/java/com/impact/analyser/report/ReportGenerator.java
+++ src/main/java/com/impact/analyser/report/ReportGenerator.java
@@ -3,6 +3,7 @@ package com.impact.analyser.report;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,12 +17,11 @@ import java.util.List;
 public class ReportGenerator {
 
     public void generateReport(List<JsonObject> jsonObjects) throws IOException {
-        String indexHtmlPath = this.getClass().getResource("/app/index.html").getPath();
+        String indexHtml = IOUtils.toString(this.getClass().getResourceAsStream("/app/index.html"), Charset.defaultCharset());
         String jsonReport = new Gson().toJson(jsonObjects);
-        String baseDirPath = System.getProperty("usr.dir");
+        String baseDirPath = System.getProperty("user.dir");
         File impactAnalysisDir = Paths.get(baseDirPath,"impact").toFile();
         FileUtils.forceMkdir(impactAnalysisDir);
-        String indexHtml = FileUtils.readFileToString(new File(indexHtmlPath), Charset.defaultCharset());
         FileUtils.writeStringToFile(Paths.get(impactAnalysisDir.getAbsolutePath(), "report.json").toFile(), jsonReport, Charset.defaultCharset());
         FileUtils.writeStringToFile(Paths.get(impactAnalysisDir.getAbsolutePath(), "index.html").toFile(), indexHtml, Charset.defaultCharset());
 
